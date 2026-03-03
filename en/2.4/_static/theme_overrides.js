44929dd1afe9144c5e876d80785a437ad07c5ecf
Doug Burks <doug.burks@securityonionsolutions.com>
2026-03-03 12:04:27 -0500

    add a static copy of 2.4 docs

---
+++ en/2.4/_static/theme_overrides.js
@@ -0,0 +1,18 @@
+const banner_text = `
+NOTICE: You are viewing documentation for a development version of Security Onion.
+<br>
+View the <a href="https://docs.securityonion.net/">documentation for the latest stable release</a>.
+`;
+
+function show_banner() {
+    if (READTHEDOCS_DATA['version'] != 'dev') return;
+
+    const banner = document.createElement('div');
+    banner.className = "so-banner";
+    banner.innerHTML = banner_text;
+    const rst = document.getElementsByClassName('rst-content')[0];
+    console.log('rst=' + rst);
+    rst.prepend(banner);
+}
+
+window.setTimeout(show_banner, 100);
